44cd51ccf2b4e5f4b300d4ae70f55deadc6fc5e5
MaybeBroken <davidsponseller123@gmail.com>
2024-10-12 11:43:23 -0700

    add nodeJs support for LocalTunnel, modify hosting to be Ngrok only

---
+++ src/loca.zsh
@@ -0,0 +1,7 @@
+
+    #!/bin/zsh
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    npx localtunnel -p 8765 -s maybebroken
+    
